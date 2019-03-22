4db6861a70c3f05265ce1476fdae3e9bc46f3ac4
OlegBlack <demon_cs@ukr.net>
2019-03-22 22:56:06 +0200

    Revert "Kmp"

--- highlighter/__init__.py
+++ highlighter/__init__.py
@@ -4,7 +4,6 @@ date: 12.12.2012
 author smith@example.com
 license: MIT"""
 
-import re
 from flask import Flask, render_template, request, Markup
 
 
@@ -32,48 +31,9 @@ def create_app():
         """Markup given text.
         @:param text - string text to be marked
         @:return marked text, e.g., <mark>highlighted text</mark>."""
+        result = text
 
-        result = "<mark>" + text + "</mark>"
-
-        return result
-
-    def prefix(sub_string):
-        """Return array of p(i) - list of prefix function result"""
-        p = [0, ]
-        j = 0
-        i = 1
-        while i < len(sub_string):
-            if sub_string[i] == sub_string[j]:
-                p.append(j + 1)
-                i += 1
-                j += 1
-            else:
-                if j == 0:
-                    p.append(0)
-                    i += 1
-                else:
-                    j = p[j - 1]
-        return p
-
-    def kmp(s, sub_s):
-        """Find substring in string by Knuth-Morris-Pratt algorithm
-        and return a list of first index of substring in string"""
-        k = 0
-        j = 0
-        p = prefix(sub_s)
-        result = []
-
-        while k < len(s):
-            if sub_s[j] == s[k]:
-                k += 1
-                j += 1
-                if j == len(sub_s):
-                    result.append(k - len(sub_s))
-                    j = 0
-            elif j == 0:
-                k += 1
-            else:
-                j = p[j - 1]
+        # TODO: add an implementation
 
         return result
 
@@ -81,22 +41,9 @@ def create_app():
         """Markup searched string in given text.
         @:param text - string text to be processed
         @:return marked text, e.g., "sample text <mark>highlighted part</mark> rest of the text"."""
+        result = text
 
-        # result = re.sub(re.compile(expr), markup_text(expr), text)
-        # result = text.replace(text, markup_text(expr))
-
-        length_sub_s = len(expr)
-        i = kmp(text, expr)
-        n = 0
-        j = 0
-        ch_sub = markup_text(expr)
-        result = ''
-
-        while n < len(i):
-            result += text[j:i[n]] + ch_sub
-            j = len(text[:i[n]]) + length_sub_s
-            n += 1
-        result += text[j:]
+        # TODO: add an implementation
 
         return result
 
